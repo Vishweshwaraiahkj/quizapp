38758db3962b6a3ccc467c8c3fc744c49b6aef4d
Vishweshwarayya <k.vishu@outlook.com>
2020-10-06 18:13:06 +0530

    feat: qas: Added timed questions

--- components/exams/ExamStart.vue
+++ components/exams/ExamStart.vue
@@ -1,26 +1,39 @@
 <template>
   <b-container fluid="sm" class="ExamBoard">
     <div v-if="isLoading" class="is_loading">Loading Data!</div>
-    <b-row v-else>
+    <b-row v-else class="dataBox">
       <h1>{{ examData.name }}</h1>
       <div class="w-100">
-        <pre>{{ examData }}</pre>
+        <SingleQuestion
+          v-if="currentQuestion"
+          :current-question="currentQuestion"
+        />
+        <div v-else><h1>You are done with exam! Thank you :)</h1></div>
       </div>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import SingleQuestion from '@/components/exams/SingleQuestion'
 export default {
   name: 'ExamStart',
+  components: {
+    SingleQuestion
+  },
   props: {
     examId: {
       type: Number,
       default: undefined
     }
   },
-  data: () => ({}),
+  data: () => ({
+    index: 0
+  }),
   computed: {
+    examStartTime() {
+      return this.getDateTime()
+    },
     examData() {
       const data = this.$store.state.exams.examFullData
         ? this.$store.state.exams.examFullData
@@ -34,10 +47,51 @@ export default {
       } else {
         return true
       }
+    },
+    currentQuestion() {
+      const i = this.index
+      if (this.examData && this.examData.sections)
+        return this.examData.sections[0].questions[i]
+      return {}
     }
   },
   created() {
     this.$store.dispatch('exams/getExamFullData', this.examId)
+    const timer = () => {
+      if (
+        this.index ===
+        (this.examData.sections && this.examData.sections[0].questions.length)
+      ) {
+        clearInterval(myVar)
+      }
+      const currentTime = this.getDateTime()
+      if (
+        // eslint-disable-next-line
+        currentTime.data === currentTime.dat &&
+        // eslint-disable-next-line
+        currentTime.time === currentTime.time &&
+        this.index <=
+          (this.examData.sections && this.examData.sections[0].questions.length)
+      ) {
+        this.index = ++this.index
+      }
+    }
+    const myVar = setInterval(timer, 60000)
+  },
+  methods: {
+    getDateTime() {
+      const today = new Date()
+      const date =
+        today.getFullYear() +
+        '-' +
+        (today.getMonth() + 1) +
+        '-' +
+        today.getDate()
+      const time =
+        today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+      const dateTime = date + ' ' + time
+      return { dateTime, date, time }
+    }
   }
 }
 </script>
@@ -49,7 +103,7 @@ export default {
   background-color: #fff;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.18);
   border-bottom: 1px solid #e0e0e0;
-  padding: 15px;
+  padding: 1rem;
   margin: 2rem auto;
 }
 
---
+++ components/exams/SingleQuestion.vue
@@ -0,0 +1,64 @@
+<template>
+  <b-container fluid="sm" class="p-3 singleQuestion">
+    <h3 :id="'question_' + currentQuestion.qunId" class="question">
+      {{ currentQuestion.qun }}
+    </h3>
+    <hr />
+    <div
+      v-for="(answer, key, index) in currentQuestion.options"
+      :id="'answer_' + currentQuestion.qunId + '_' + key"
+      :key="index"
+      class="answers"
+    >
+      <label>{{ index + 1 }} ({{ key }}):</label>
+      <span>{{ answer }}</span>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'SingleQuestion',
+  props: {
+    currentQuestion: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data: () => ({
+    isActive: true
+  })
+}
+</script>
+
+<style>
+.singleQuestion {
+  display: block;
+  padding: 0 !important;
+}
+.question {
+  border-radius: 0;
+  background-color: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.18);
+  border-bottom: 1px solid #e0e0e0;
+  padding: 15px;
+}
+.answers {
+  justify-content: flex-start;
+  border-radius: 0;
+  background-color: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.18);
+  border-bottom: 1px solid #e0e0e0;
+  padding: 0.75rem;
+  display: flex;
+  flex: 1;
+  margin-bottom: 0.625rem;
+}
+.answers label {
+  padding: 0 1rem 0 0;
+  margin-bottom: 0;
+}
+.answers span {
+  width: auto;
+}
+</style>
